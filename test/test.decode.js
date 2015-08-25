8b87cba92c66ebd3757dff9541b43876c4d5214f
kgryte <kgryte@gmail.com>
2015-08-24 22:52:40 -0700

    [UPDATE] s/encode/decode/.

--- test/test.decode.js
+++ test/test.decode.js
@@ -4,7 +4,7 @@
 // MODULES //
 
 var chai = require( 'chai' ),
-	encode = require( './../lib/encode.js' );
+	decode = require( './../lib/decode.js' );
 
 
 // VARIABLES //
@@ -15,29 +15,29 @@ var expect = chai.expect,
 
 // TESTS //
 
-describe( 'encode', function tests() {
+describe( 'decode', function tests() {
 
 	it( 'should export a function', function test() {
-		expect( encode ).to.be.a( 'function' );
+		expect( decode ).to.be.a( 'function' );
 	});
 
-	it( 'should convert a UTF-8 encoded string to a specified encoding', function test() {
+	it( 'should convert a UTF-8 string to a specified encoding', function test() {
 		var str;
 
-		str = encode( 'beep', 'base64' );
+		str = decode( 'beep', 'base64' );
 		assert.strictEqual( str, new Buffer( 'beep' ).toString( 'base64' ) );
 
-		str = encode( 'beep', 'ascii' );
+		str = decode( 'beep', 'ascii' );
 		assert.strictEqual( str, new Buffer( 'beep' ).toString( 'ascii' ) );
 	});
 
 	it( 'should return the input string if the specified encoding is either `utf8` or `buffer`', function test() {
 		var str;
 
-		str = encode( 'beep', 'utf8' );
+		str = decode( 'beep', 'utf8' );
 		assert.strictEqual( str, 'beep' );
 
-		str = encode( 'beep', 'buffer' );
+		str = decode( 'beep', 'buffer' );
 		assert.strictEqual( str, 'beep' );
 	});
 
